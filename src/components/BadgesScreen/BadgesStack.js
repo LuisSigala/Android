515bdddc8bb163ddfba57523ecc084fe363b02a6
LuisSigala <a6519150031@utch.edu.mx>
2021-07-01 23:11:56 -0600

    Android

---
+++ src/components/BadgesScreen/BadgesStack.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import BadgeLanding from '../BadgesLanding/BadgeLanding'
+import BadgesScreen from './BadgesScreen'
+import BadgesDetail from '../BadgesDetail/BadgesDetail'
+import BadgesEdit from '../BadgesEdit/BadgesEdit'
+import Colors from '../../res/Colors'
+
+const Stack = createStackNavigator(); 
+
+const BadgesStack = () => {
+    return(
+        <Stack.Navigator
+            screenOptions={{ 
+                headerStyle:{
+                    backgroundColor: Colors.blackPearl,
+                    shadowColor: Colors.blackPearl,
+                },
+                headerTintColor: Colors.white,
+             }}
+            >
+            <Stack.Screen 
+            name='Landing' 
+            component={BadgeLanding} 
+            options={{headerShown: false}}
+            />
+            <Stack.Screen name="Badges" component={BadgesScreen} />
+            <Stack.Screen name="BadgesDetail" component={BadgesDetail} />
+            <Stack.Screen name="BadgesEdit" component={BadgesEdit} />
+        </Stack.Navigator>
+    )
+}
+
+export default BadgesStack;
